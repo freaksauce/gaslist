42a725e0e67c5b26f91a9088b2df1bb623435db6
Jon Bloomer <jon@freaksauce.com>
2015-03-31 09:48:19 +0000

    added simple login and add a list form, not hooked up to collection yet

--- gaslist-simple.js
+++ gaslist-simple.js
@@ -1,9 +1,28 @@
 if (Meteor.isClient) {
-
-  Template.loggedin.helpers({
-    name: function() {
-      console.log(this.userId);
-      return this.userId;
+  gaslistsCollection = new Mongo.Collection('gaslistsCollection');
+  
+  Template.userGaslists.helpers({
+    gaslists: function() {
+      this._id = Meteor.userId();
+      Session.set('currentUser', this._id);
+      var userGasLists = gaslistsCollection.find({_id: this._id});
+      var count = userGasLists.count();
+      console.log(userGasLists);
+      console.log(count);
+      Session.set('userGasLists', userGasLists);
+      if (count > 0) {
+        return true;
+      }else{
+        return false;
+      }
+    }
+  });
+  
+  Template.userGaslists.events({
+    'submit #createGaslist': function(evt){
+        evt.preventDefault();
+        var val = evt.target.gaslistName.value;
+        console.log('submit: '+val);
     }
   });
 
@@ -12,5 +31,6 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    
   });
 }
