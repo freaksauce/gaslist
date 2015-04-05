cc3535aafc46339513ffec4bbb75c4b57790117e
Jon Bloomer <jon@freaksauce.com>
2015-04-05 18:38:40 +1000

    removed insecure and added Methods and Events, including delete list

--- client/client.js
+++ client/client.js
@@ -6,8 +6,8 @@ Template.userGaslists.helpers({
     Session.set('currentUser', this._id);
     var userGasLists = gaslistsCollection.find({createdBy: this._id});
     var count = userGasLists.count();
-    console.log(userGasLists.fetch());
-    console.log(count);
+    // console.log(userGasLists.fetch());
+    // console.log(count);
     if (count > 0) {
       return true;
     }else{
@@ -23,6 +23,7 @@ Template.userGaslists.events({
   'submit #createGaslist': function(evt){
       evt.preventDefault();
       gaslistName = evt.target.gaslistName.value;
+      Session.set('gaslistName', gaslistName);
       console.log('submit: '+gaslistName);
 
       var checkExists = gaslistsCollection.find({
@@ -30,15 +31,38 @@ Template.userGaslists.events({
       });
       console.log(checkExists);
       // if (!checkExists) {
-        gaslistsCollection.insert({
-          listName: gaslistName,
-          createdBy: Meteor.userId()
+        var addList = Meteor.call('addGaslist', gaslistName, function(err, result) {
+          if (err) {
+            console.log('[ERROR]:\n');
+            console.log(err);
+            return;
+          }
+          if (result) {
+            $('input[name=gaslistName]').val('');
+            $('.msg').text('Your gaslist "'+Session.get('gaslistName')+'" was created.');
+            Session.set('gaslistName', '');
+            console.log('list added');
+          }
         });
-        $('input[name=gaslistName]').val('');
-        $('.msg').text('Your gaslist "'+gaslistName+'" was created.')
+//
       // }else{
       //   $('.errorMsg').text('This gaslist name is already in use');
       //   $('input[name=gaslistName]').val('');
       // }
+  },
+  'click .delete': function(evt) {
+    console.log('delete');
+    evt.preventDefault();
+    var listId = evt.target.getAttribute('data-id');
+    Meteor.call('removeGaslist', listId, function(err, result) {
+      if (err) {
+        console.log('[ERROR]:\n');
+        console.log(err);
+        return;
+      }
+      if (result) {
+        console.log('Result: '+result);
+      }
+    });
   }
 });
--- server/server.js
+++ server/server.js
@@ -3,3 +3,18 @@ Meteor.startup(function () {
    return gaslistsCollection.find()
   });
 });
+
+Meteor.methods({
+  addGaslist: function(gaslistName) {
+    var currentUserId = Meteor.userId();
+    return gaslistsCollection.insert({
+      listName: gaslistName,
+      createdBy: currentUserId
+    });
+  },
+  removeGaslist: function(gaslistId) {
+    return gaslistsCollection.remove({
+      _id: gaslistId
+    })
+  }
+});
