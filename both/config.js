d6e665f3db68b82dcc30b6ab572d3a679af724a9
Jon Bloomer <jon@freaksauce.com>
2015-04-05 21:12:11 +1000

    fixed routes and layouts now using yield

--- both/config.js
+++ both/config.js
@@ -1,18 +1,12 @@
 gaslistsCollection = new Mongo.Collection('gaslistsCollection');
 
 Router.route('/', function () {
+  this.layout('layoutMain');
   this.render('userGaslists');
 });
 
-Router.route('list', {
-  path: '/gaslist/:_id',
-  layoutTemplate: 'showGaslist',
-  action: function() {
-    var list = gaslistsCollection.findOne({_id: this.params._id});
-    this.render('ShowGaslist', {data: list});
-  }
+Router.route('/gaslist/:_id', function () {
+  this.layout('layoutMain');
+  var list = gaslistsCollection.findOne({_id: this.params._id});
+  this.render('ShowGaslist', {data: list});
 });
-// Router.route('/gaslist/:_id', function () {
-//   var list = gaslistsCollection.findOne({_id: this.params._id});
-//   this.render('ShowGaslist', {data: list});
-// });
