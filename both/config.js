d5fd42d6a2e00e5959e4bc3bfd834b582e0ca0a2
Jon Bloomer <jon@freaksauce.com>
2015-04-05 20:37:58 +1000

    tidied up templates to use Iron Router

--- both/config.js
+++ both/config.js
@@ -1,1 +1,14 @@
 gaslistsCollection = new Mongo.Collection('gaslistsCollection');
+
+Router.route('/', function () {
+  this.render('gaslistContent');
+});
+
+// Router.route('/gaslist', function () {
+//   this.render('Items');
+// });
+
+Router.route('/gaslist/:_id', function () {
+  var list = gaslistCollection.findOne({_id: this.params._id});
+  this.render('ShowGaslist', {data: list});
+});
